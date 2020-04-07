052abdc7d9b95a389e24b7dadb94a9eacbfc42b7
Dylan <dehall@mitre.org>
2020-04-07 15:20:06 -0400

    mCODE 1.0 mapper, plus some QoL fixes

--- src/utils/index.js
+++ src/utils/index.js
@@ -1,3 +1,4 @@
 const common = require('./common');
 const mcodeUtils09 = require('./mcodeUtils09');
-module.exports = {...common, mcodeUtils09};
+const mcodeUtils10 = require('./mcodeUtils10');
+module.exports = {...common, mcodeUtils09, mcodeUtils10};
