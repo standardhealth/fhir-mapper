a5e451292e2c112ac0174ef595ba69fddf86ef45
Dylan Mendelowitz <dmendelowitz@mitre.org>
2022-04-13 15:09:18 -0400

    SyntheaToSTU2 initial setup and TNM and Staging STU2 updates

--- src/mapping/mappers/index.js
+++ src/mapping/mappers/index.js
@@ -1,6 +1,7 @@
 const SyntheaToV05 = require('./SyntheaToV05');
 const SyntheaToV09 = require('./SyntheaToV09');
 const SyntheaToV10 = require('./SyntheaToV10');
+const SyntheaToSTU2 = require('./SyntheaToSTU2');
 const Filterer = require('./Filterer');
 const Cerner = require('./Cerner');
 const OpenEpicMapper = require('./OpenEpic');
@@ -9,6 +10,7 @@ module.exports = {
   SyntheaToV05,
   SyntheaToV09,
   SyntheaToV10,
+  SyntheaToSTU2,
   Filterer,
   Cerner,
   OpenEpicMapper
